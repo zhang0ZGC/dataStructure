53f7ac6041bf0f4a25163d20da3263e79120db88
XM <xm@tqzgc.com>
2020-11-24 19:51:21 +0800

    Promise

---
+++ src/other/Promise.js
@@ -0,0 +1,137 @@
+"use strict";
+/**
+ * @link https://malcolmyu.github.io/malnote/2015/06/12/Promises-A-Plus
+ **/
+exports.__esModule = true;
+/**
+ * Promise决议程序
+ * @param {MyPromise} promise2
+ * @param x
+ * @param resolve
+ * @param reject
+ */
+function resolutionProducer(promise2, x, resolve, reject) {
+    if (promise2 === x) {
+        reject(new TypeError('Error'));
+        // } else if(x instanceof MyPromise) {
+        //   x.then((xValue: any) => {
+        //     resolutionProducer(promise2, xValue, resolve, reject);
+        //   }, (reason: any) => reject(reason));
+    }
+    else if (x !== null && typeof x === 'object' || typeof x === 'function') {
+        try {
+            var then = x.then;
+            if (typeof then === 'function') {
+                var hasCalled_1 = false;
+                try {
+                    then.call(x, function (y) {
+                        if (!hasCalled_1) {
+                            hasCalled_1 = true;
+                            resolutionProducer(promise2, y, resolve, reject);
+                        }
+                    }, function (reason) {
+                        if (!hasCalled_1) {
+                            hasCalled_1 = true;
+                            reject(reason);
+                        }
+                    });
+                }
+                catch (e) {
+                    reject(e);
+                }
+            }
+            else {
+                resolve(x);
+            }
+        }
+        catch (e) {
+            reject(e);
+        }
+    }
+    else {
+        resolve(x);
+    }
+}
+// 还是用原Promise来作为异步任务队列哈哈哈哈哈。。。
+// const defer = typeof Promise === 'function' ? Promise.prototype.then.bind(Promise.resolve()) : setTimeout;
+var defer = setTimeout;
+var MyPromise = /** @class */ (function () {
+    function MyPromise(executor) {
+        this.status = 'pending';
+        this.onFulfilledQueue = [];
+        this.onRejectedQueue = [];
+        var self = this;
+        function resolve(value) {
+            if (self.status === 'pending') {
+                self.status = 'fulfilled';
+                self.value = value;
+                self.onFulfilledQueue.forEach(function (func) { return func(value); });
+            }
+        }
+        function reject(e) {
+            if (self.status === 'pending') {
+                self.status = 'rejected';
+                self.reason = e;
+                self.onRejectedQueue.forEach(function (func) { return func(e); });
+            }
+        }
+        try {
+            executor(resolve, reject);
+        }
+        catch (e) {
+            reject(e);
+        }
+    }
+    MyPromise.prototype.then = function (onFulfilled, onRejected) {
+        if (typeof onFulfilled !== 'function')
+            onFulfilled = function onResolved(v) { return v; };
+        if (typeof onRejected !== 'function')
+            onRejected = function onRejected(e) { throw e; };
+        var self = this;
+        function resolveByStatus(resolve, reject) {
+            defer(function () {
+                try {
+                    var x = onFulfilled(self.value);
+                    resolutionProducer(promise2, x, resolve, reject);
+                }
+                catch (e) {
+                    reject(e);
+                }
+            });
+        }
+        function rejectByStatus(resolve, reject) {
+            defer(function () {
+                try {
+                    var x = onRejected(self.value);
+                    resolutionProducer(promise2, x, resolve, reject);
+                }
+                catch (e) {
+                    reject(e);
+                }
+            });
+        }
+        var promise2 = new MyPromise((function (resolve, reject) {
+            switch (self.status) {
+                case "pending":
+                    self.onFulfilledQueue.push(function () {
+                        resolveByStatus(resolve, reject);
+                    });
+                    self.onRejectedQueue.push(function () {
+                        rejectByStatus(resolve, reject);
+                    });
+                    break;
+                case "fulfilled":
+                    resolveByStatus(resolve, reject);
+                    break;
+                case "rejected":
+                    rejectByStatus(resolve, reject);
+                    break;
+                default:
+                    break;
+            }
+        }));
+        return promise2;
+    };
+    return MyPromise;
+}());
+exports["default"] = MyPromise;
---
+++ src/other/Promise.ts
@@ -0,0 +1,154 @@
+/**
+ * @link https://malcolmyu.github.io/malnote/2015/06/12/Promises-A-Plus
+ **/
+
+type PromiseExecutor<T> = (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void;
+type PromiseStatus = 'pending' | 'fulfilled' | 'rejected';
+
+interface IMyPromise<T> {
+  then<TResult1 = T, TResult2 = never>(onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): MyPromise<TResult1 | TResult2>;
+}
+
+/**
+ * Promise决议程序
+ * @param {MyPromise} promise2
+ * @param x
+ * @param resolve
+ * @param reject
+ */
+function resolutionProducer(promise2: MyPromise, x: any, resolve: Function, reject: Function) {
+  if (promise2 === x) {
+    reject(new TypeError('Error'))
+  // } else if(x instanceof MyPromise) {
+  //   x.then((xValue: any) => {
+  //     resolutionProducer(promise2, xValue, resolve, reject);
+  //   }, (reason: any) => reject(reason));
+  } else if (x !== null && typeof x === 'object' || typeof x === 'function') {
+    try {
+      const then = x.then;
+
+      if (typeof then === 'function') {
+        let hasCalled = false;
+        try {
+          then.call(x,
+            (y: any) => {
+              if (!hasCalled) {
+                hasCalled = true;
+                resolutionProducer(promise2, y, resolve, reject);
+              }
+            },
+            (reason: any) => {
+              if (!hasCalled) {
+                hasCalled = true;
+                reject(reason);
+              }
+            },
+          );
+        } catch (e) {
+          reject(e);
+        }
+      } else {
+        resolve(x);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  } else {
+    resolve(x);
+  }
+}
+
+// 还是用原Promise来作为异步任务队列哈哈哈哈哈。。。
+// const defer = typeof Promise === 'function' ? Promise.prototype.then.bind(Promise.resolve()) : setTimeout;
+const defer = setTimeout;
+
+class MyPromise<T=any> implements IMyPromise<T>{
+  protected value?: T | PromiseLike<T>;
+  protected reason?: unknown;
+  protected status: PromiseStatus = 'pending';
+  protected onFulfilledQueue: Function[] = [];
+  protected onRejectedQueue: Function[] = [];
+
+  constructor(executor: PromiseExecutor<T>) {
+    const self = this;
+
+    function resolve(value?: T | PromiseLike<T>) {
+      if (self.status === 'pending') {
+        self.status = 'fulfilled';
+        self.value = value;
+        self.onFulfilledQueue.forEach(func => func(value));
+      }
+    }
+
+    function reject(e: unknown) {
+      if (self.status === 'pending') {
+        self.status = 'rejected';
+        self.reason = e;
+        self.onRejectedQueue.forEach(func => func(e));
+      }
+    }
+
+    try{
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e);
+    }
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
+    onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
+  ){
+    if (typeof onFulfilled !== 'function') onFulfilled = function onResolved(v: any){return v};
+    if (typeof onRejected !== 'function') onRejected = function onRejected(e: any){throw e};
+
+    const self = this;
+
+    function resolveByStatus(resolve: Function, reject: Function) {
+      defer(() => {
+        try {
+          const x = onFulfilled!(self.value as T);
+          resolutionProducer(promise2, x, resolve, reject);
+        } catch (e) {
+          reject(e);
+        }
+      })
+    }
+
+    function rejectByStatus(resolve: Function, reject: Function) {
+      defer(() => {
+        try {
+          const x = onRejected!(self.value);
+          resolutionProducer(promise2, x, resolve, reject);
+        } catch (e) {
+          reject(e);
+        }
+      })
+    }
+
+    const promise2 = new MyPromise<TResult1 | TResult2>(((resolve, reject) => {
+      switch (self.status) {
+        case "pending":
+          self.onFulfilledQueue.push(() => {
+            resolveByStatus(resolve, reject);
+          });
+          self.onRejectedQueue.push(() => {
+            rejectByStatus(resolve, reject);
+          })
+          break;
+        case "fulfilled":
+          resolveByStatus(resolve, reject);
+          break;
+        case "rejected":
+          rejectByStatus(resolve, reject);
+          break;
+        default:
+          break;
+      }
+    }));
+
+    return promise2;
+  }
+}
+
+export default MyPromise;
